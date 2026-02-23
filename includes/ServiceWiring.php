3575a19af848a96a7a0d2485b60f9c606f0b55c7
Fomafix <fomafix@googlemail.com>
2026-02-23 16:35:39 +0000

    Improve ServiceWiring
    * Add @phpcs-require-sorted-array and sort array.
    * Add PHP return type declarations.
    
    Change-Id: I0e83868aa820661d02a021f82e545b4c4f9a24bf

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -4,14 +4,19 @@ use MediaWiki\Extension\CollabPads\CollabRevisionManager;
 use MediaWiki\Extension\CollabPads\CollabSessionManager;
 use MediaWiki\MediaWikiServices;
 
+/** @phpcs-require-sorted-array */
 return [
-	'CollabPadsCollabSessionManager' => static function ( MediaWikiServices $services ) {
+	'CollabPadsCollabRevisionManager' => static function (
+		MediaWikiServices $services,
+	): CollabRevisionManager {
+		return new CollabRevisionManager( $services->getDBLoadBalancer() );
+	},
+	'CollabPadsCollabSessionManager' => static function (
+		MediaWikiServices $services,
+	): CollabSessionManager {
 		return new CollabSessionManager(
 			$services->getConnectionProvider(),
 			$services->getObjectCacheFactory()
 		);
 	},
-	'CollabPadsCollabRevisionManager' => static function ( MediaWikiServices $services ) {
-		return new CollabRevisionManager( $services->getDBLoadBalancer() );
-	}
 ];
